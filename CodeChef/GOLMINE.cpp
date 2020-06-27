dcdc3b0d462b41102fb3331245f0d0eea559e8b4
KausBoss <ktkaustubh4@gmail.com>
2020-06-27 23:44:22 +0530

    27 June

---
+++ CodeChef/GOLMINE.cpp
@@ -0,0 +1,59 @@
+/*
+
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll           long long int
+#define mp           make_pair
+#define pb           push_back
+#define fi           first
+#define si           second
+#define fastIO       ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define F(a,n)       for(int i=0;i<n;i++){cin>>a[i];}
+#define F1(a,n)      for(int i=1;i<=n;i++){cin>>a[i];}
+#define P(a,n)       for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define P1(a,n)      for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define NF(a,n,m)    for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m)   for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m)   for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+const int nax = 1e7;
+const int mod = 1e9+7;
+
+void func(){
+	ll n;
+	cin>>n;
+	double *a = new double[n];
+	double *b = new double[n];
+	double *g = new double[n];
+	for(int i=0; i<n; i++){
+		cin>>g[i]>>a[i]>>b[i];
+	}
+
+	double scoreA=0, scoreB=0;
+
+	for(int i=0; i<n; i++){
+		double x = g[i]*b[i];
+		x /= (a[i] + b[i]);
+		double y = g[i] - x;
+		scoreA += x;
+		scoreB += y;
+	}
+	cout<<fixed<<setprecision(6)<<scoreA<<" "<<scoreB;
+}
+
+int main(){
+fastIO
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+	int t;
+	cin>>t;
+	while(t--){
+		func();
+		cout<<endl;
+	}
+}

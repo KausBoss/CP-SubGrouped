069822b5b2a22516949e6dda0edeff17587144ec
KausBoss <ktkaustubh4@gmail.com>
2020-07-02 04:13:32 +0530

    2 July

--- CodeForces/1371D.cpp
+++ CodeForces/1371D.cpp
@@ -17,13 +17,28 @@ using namespace std;
 #define P1(a,n)      for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
 #define NF(a,n,m)    for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
 #define NF1(a,n,m)   for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
-#define PNF(a,n,m)   for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF(a,n,m)   for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j];}cout<<endl;};
 #define PNF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
 const int nax = 1e7;
 const int mod = 1e9+7;
 
 void func(){
-	
+	int n, k, p=0, q=0;
+	cin>>n>>k;
+	if(k%n == 0){
+		cout<<0<<endl;
+	}
+	else{cout<<2<<endl;}
+	vector<vector<bool>> a(n, vector<bool>(n, 0));
+
+	for(int i=0; i<k; i++){
+		a[p][q] = 1;
+		p++; q=(q+1)%n;
+		if(p == n){
+			p=0; q=(q+1)%n;
+		}
+	}
+	PNF(a, n, n);
 }
 
 int main(){
@@ -32,9 +47,10 @@ fastIO
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-	int t;
-	cin>>t;
-	while(t--){
-		func();
-	}
+    int t;
+    cin>>t;
+
+    while(t--){
+    	func();
+    }
 }

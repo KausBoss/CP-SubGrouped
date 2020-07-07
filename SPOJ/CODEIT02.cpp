e5f60c8b194a3780cbc3585ffa41e535a5852805
KausBoss <ktkaustubh4@gmail.com>
2020-07-07 12:29:06 +0530

    7 July

---
+++ SPOJ/CODEIT02.cpp
@@ -0,0 +1,50 @@
+
+/*
+
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll           long long int
+#define mp           make_pair
+#define pb           push_back
+#define fi           first
+#define si           second
+#define fastIO       ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define F(a,n)       for(int i=0;i<n;i++){cin>>a[i];}
+#define F1(a,n)      for(int i=1;i<=n;i++){cin>>a[i];}
+#define P(a,n)       for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define P1(a,n)      for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define NF(a,n,m)    for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m)   for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m)   for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m)  for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+const int nax = 1e7;
+const int mod = 1e9+7;
+ll n, *a, k;
+
+ll func(ll i, ll num, ll k){
+	//base case
+	if(k == 0 || i == n){
+		return (k==0) ? num : INT_MIN;
+	}
+	//base case
+	return max(func(i+1, num^a[i], k-1), func(i+1, num, k));
+}
+
+int main(){
+fastIO
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+	ll t;
+	cin>>t;
+	while(t--){
+		cin>>n>>k;
+		a = new ll[n];
+		F(a, n);
+		cout<<func(0, 0, k)<<endl;
+	}
+}

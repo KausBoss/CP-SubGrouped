34eb2150040fc18cdcf3a4afe747cbd68caa6122
KausBoss <ktkaustubh4@gmail.com>
2020-07-08 18:09:38 +0530

    8 July

---
+++ Algorithms/Deletion in LinkedList.cpp
@@ -0,0 +1,129 @@
+/*
+
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll           long long int
+#define mp           make_pair
+#define pb           push_back
+#define fi           first
+#define si           second
+#define fastIO       ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define F(a,n)       for(int i=0;i<n;i++){cin>>a[i];}
+#define F1(a,n)      for(int i=1;i<=n;i++){cin>>a[i];}
+#define P(a,n)       for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define P1(a,n)      for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define NF(a,n,m)    for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m)   for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m)   for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m)  for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+const int nax = 1e7;
+const int mod = 1e9+7;
+
+class Node{
+public:
+	int data;
+	Node* next;
+	//constructors
+	Node(){}
+	Node(int data){
+		this->data =data;
+		next = NULL;
+	}
+};
+
+int lengthLL(Node *head){
+	int length=0;
+	while(head != NULL){
+		head = head->next;
+		length++;
+	}
+	return length;
+}
+
+void headInsert(Node *&head, int data){
+	Node *n= new Node(data);
+	n->next = head;
+	head = n;
+}
+
+void deleteHead(Node *&head){
+	if(head == NULL){return;}
+	Node *temp = head;
+	head = head->next;
+	delete temp;
+}
+
+void deleteTail(Node *head){
+	if(head == NULL){return;}
+
+	Node *temp = head->next;
+	while(temp->next != NULL){
+		temp = temp->next;
+		head = head->next;
+	}
+	head->next = NULL;
+	delete temp;
+}
+
+void deleteMiddle(Node *&head, int pos){
+	//case 1
+	if(head == NULL || pos == 1){
+		deleteHead(head);
+		return;
+	}
+
+	//case 2
+	if(pos >= lengthLL(head)){
+		deleteTail(head);
+		return;
+	}
+
+	//case 3
+	int len=1;
+	Node *temp = head;
+	while(len < pos-1){
+		temp = temp->next;
+		len++;
+	}
+	Node *n= temp->next;
+	temp->next = temp->next->next;
+	delete n;
+}
+
+void printLL(Node *head){
+	while(head != NULL){
+		cout<<head->data<<"->";
+		head = head->next;
+	}
+	cout<<endl;
+
+}
+
+int main(){
+fastIO
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    //preparing a Linked List
+    Node  *head = NULL;
+	//insert at head
+	for(int i=0; i<=7; i++){
+		headInsert(head, i);
+	}
+
+	//delete from head
+	deleteHead(head);
+	printLL(head);
+
+	//delete from tail
+	deleteTail(head);
+	printLL(head);
+
+	//delete from middle
+	deleteMiddle(head, 4);
+	printLL(head);
+}

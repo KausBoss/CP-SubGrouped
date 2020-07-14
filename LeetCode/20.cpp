babfddd8d71843550b4aab7dd876c72bc11900ee
KausBoss <ktkaustubh4@gmail.com>
2020-07-14 15:55:00 +0530

    14 July

---
+++ LeetCode/20.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i=0; i<s.length(); i++){
+        	if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
+        		st.push(s[i]);
+        	}
+        	else{
+                if(st.empty()){return false;}
+        		if(s[i]==')' && st.top()!='('){return false;}
+        		if(s[i]==')' && st.top()=='('){st.pop(); continue;}
+        		if(s[i]=='}' && st.top()!='{'){return false;}
+        		if(s[i]=='}' && st.top()=='{'){st.pop(); continue;}
+        		if(s[i]==']' && st.top()!='['){return false;}
+        		if(s[i]==']' && st.top()=='['){st.pop(); continue;}
+        	}
+        }
+        if(!st.empty()){return false;}
+        return true;
+    }
+};

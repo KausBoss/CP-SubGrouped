bf349d9f1be61f52ab3170a8522ad2216752d7c6
KausBoss <ktkaustubh4@gmail.com>
2020-07-23 03:27:03 +0530

    23 July

---
+++ Algorithms/BST check if a BST.cpp
@@ -0,0 +1,101 @@
+/*
+
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll          long long int
+#define vi 			 vector<int>
+#define pb           push_back
+#define fi           first
+#define si           second
+#define pii 		 pair<int,int>
+#define vii 		 vector<pair<int,int>>
+#define F(a,n)       for(int i=0;i<n;i++){cin>>a[i];}
+#define P(a,n)       for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define fastIO       ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define F1(a,n)      for(int i=1;i<=n;i++){cin>>a[i];}
+#define P1(a,n)      for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define mem(a,b)     memset(a, b, sizeof a)
+#define matrix(x) 	 vector<vector<x>>
+#define NF(a,n,m)    for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m)   for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m)   for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m)  for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+const int nax = 1e7;
+const int mn = INT_MIN;
+const int mx = INT_MAX;
+
+class Node{
+public:
+	int data;
+	Node *left;
+	Node *right;
+	Node(){}
+	Node(int data){
+		this->data = data;
+		left = right = NULL;
+	}
+	Node (int data, Node *left, Node *right){
+		this->data = data;
+		this->right = right;
+		this->left = left;
+	}
+};
+
+//Function to build bST
+Node* buildBST(Node *root, int d){
+	//base case
+	if(root == NULL){
+		return new Node(d);
+	}
+	//recursive case
+	if(d <= root->data){
+		root->left = buildBST(root->left, d);
+	}
+	else{
+		root->right = buildBST(root->right, d);
+	}
+	return root;
+}
+
+//Printing PreOrder of Tree
+void prePrint(Node* root){
+	//base case
+	if(root == NULL){
+		return;
+	}
+	//recursive case
+	cout<<root->data<<" ";
+	prePrint(root->left);
+	prePrint(root->right);
+}
+
+//check if a binary tree is bst
+bool checker(Node *root, int mini=mn, int maxi=mx){
+	//base case
+	if(root == NULL){
+		return true;
+	}
+	//recursive case
+	if(root->data < mini || root->data > maxi){
+		return false;
+	}
+	return checker(root->left, mini, root->data) && 
+		   checker(root->right, root->data, maxi);
+}
+
+int main(){
+fastIO
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+	Node* root = NULL;
+	vector<int> v{25, 20, 10, 5, 12, 22, 36, 30, 28, 140, 38, 48};
+	for(auto x:v){
+		root =buildBST(root, x);
+	}
+	cout<<checker(root);
+}

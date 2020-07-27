f8625dfccc42870518ee56d0a24eaeda7063178f
KausBoss <ktkaustubh4@gmail.com>
2020-07-28 03:23:34 +0530

    28 July

---
+++ CodeForces/996A.cpp
@@ -0,0 +1,47 @@
+/*
+
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll          long long int
+#define vi 			 vector<int>
+#define pb           push_back
+#define fi           first
+#define si           second
+#define pii 		 pair<int,int>
+#define vii 		 vector<pair<int,int>>
+#define F(a,n)       for(int i=0;i<n;i++){cin>>a[i];}
+#define P(a,n)       for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define fastIO       ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define F1(a,n)      for(int i=1;i<=n;i++){cin>>a[i];}
+#define P1(a,n)      for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define mem(a,b)     memset(a, b, sizeof a)
+#define matrix(x) 	 vector<vector<x>>
+#define NF(a,n,m)    for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m)   for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m)   for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m)  for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+const ll nax = 1e13;
+const ll mod = 1e9+7;
+
+ll currency[] = {1, 5, 10, 20, 100};
+
+int main(){
+fastIO
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+	ll n;
+	cin>>n;
+	ll ans=0;
+	for(int i=4; i>=0; i--){
+		if(currency[i] <= n){
+			ans += n/currency[i];
+			n -=  (n/currency[i])*currency[i];
+		}
+	}
+	cout<<ans;
+}

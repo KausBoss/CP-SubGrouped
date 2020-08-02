7c5b8ba0e1db8a66235c0774b5690da65976c797
KausBoss <ktkaustubh4@gmail.com>
2020-08-03 01:06:48 +0530

    2 Aug

---
+++ GFG/Three way partitioning.cpp
@@ -0,0 +1,64 @@
+/*
+
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll          long long int
+#define vi 			 vector<int>
+#define pb           push_back
+#define fi           first
+#define si           second
+#define pii 		 pair<int,int>
+#define vii 		 vector<pair<int,int>>
+#define F(a,n)       for(int i=0;i<n;i++){cin>>a[i];}
+#define P(a,n)       for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define fastIO       ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define F1(a,n)      for(int i=1;i<=n;i++){cin>>a[i];}
+#define P1(a,n)      for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define mem(a,b)     memset(a, b, sizeof a)
+#define matrix(x) 	 vector<vector<x>>
+#define NF(a,n,m)    for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m)   for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m)   for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m)  for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+const int nax = 1e7;
+const int mod = 1e9+7;
+
+void func(){
+	int n;cin>>n;
+	vector<int> A(n);
+	F(A, n);
+	int a, b;
+	cin>>a>>b;
+	int j = n-1;
+	 for(int i=0; i<n-1 && i<j; i++){
+	 	// P(A, n);
+	 	while(j>i && A[j]>b){j--;}
+	 	if(A[i] > b){
+	 		swap(A[i], A[j--]);
+	 	}
+	 }
+	 // cout<<"-----------------\n";
+	 for(int i=0; i<n-1 && i<j; i++){
+	 	while(j>i && A[j]>= a){j--;}
+	 	if(A[i] >= a){
+	 		swap(A[i], A[j]);
+	 	}
+	 }
+	 P(A, n);
+	 // return A;
+}
+
+int main(){
+fastIO
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+	int t=1;cin>>t;
+	while(t--){
+		func();
+	}
+}

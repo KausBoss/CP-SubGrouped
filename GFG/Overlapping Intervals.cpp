939c0b0d2205b90ca1b34ab4a49dff02d63fa9c8
KausBoss <ktkaustubh4@gmail.com>
2020-08-03 18:49:29 +0530

    3 Aug

---
+++ GFG/Overlapping Intervals.cpp
@@ -0,0 +1,66 @@
+/*
+
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll          long long int
+#define vi 			 vector<int>
+#define pb           push_back
+#define fi           first
+#define si           second
+#define pii 		 pair<int,int>
+#define vii 		 vector<pair<int,int>>
+#define F(a,n)       for(int i=0;i<n;i++){cin>>a[i];}
+#define P(a,n)       for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define fastIO       ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define F1(a,n)      for(int i=1;i<=n;i++){cin>>a[i];}
+#define P1(a,n)      for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define mem(a,b)     memset(a, b, sizeof a)
+#define matrix(x) 	 vector<vector<x>>
+#define NF(a,n,m)    for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m)   for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m)   for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m)  for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+const int nax = 1e7;
+const int mod = 1e9+7;
+
+void func(){
+	ll n;
+	cin>>n;
+	vector<pair<ll, ll>> p;
+	for(int i=0; i<n; i++){
+		ll x, y;
+		cin>>x>>y;
+		p.pb({x, y});
+	}
+	sort(p.begin(), p.end());
+	vector<ll>ans;
+	ans.pb(p[0].fi);
+	ll end=p[0].si;
+	for(int i=1; i<n; i++){
+		if(p[i].fi <= end){
+			end = max(p[i].si, end);
+		}
+		else{
+			ans.pb(end);
+			ans.pb(p[i].fi);
+			end = p[i].si;
+		}
+	}
+	if(ans.size()%2){ans.pb(end);}
+	P(ans, ans.size());
+}
+
+int main(){
+fastIO
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+	int t=1;cin>>t;
+	while(t--){
+		func();
+	}
+}

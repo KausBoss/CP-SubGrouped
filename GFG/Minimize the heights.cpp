df391d80c2b736f649e2e847d915d5291665c53d
KausBoss <ktkaustubh4@gmail.com>
2020-08-04 00:31:18 +0530

    3 Aug

---
+++ GFG/Minimize the heights.cpp
@@ -0,0 +1,70 @@
+/*
+
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll          long long int
+#define vi 			 vector<int>
+#define pb           push_back
+#define fi           first
+#define si           second
+#define pii 		 pair<int,int>
+#define vii 		 vector<pair<int,int>>
+#define F(a,n)       for(int i=0;i<n;i++){cin>>a[i];}
+#define P(a,n)       for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define fastIO       ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define F1(a,n)      for(int i=1;i<=n;i++){cin>>a[i];}
+#define P1(a,n)      for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define mem(a,b)     memset(a, b, sizeof a)
+#define matrix(x) 	 vector<vector<x>>
+#define NF(a,n,m)    for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m)   for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m)   for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m)  for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+const int nax = 1e7;
+const int mod = 1e9+7;
+
+void func(){
+	int n, k, *a;
+	cin>>k>>n;
+	a = new int[n];
+	F(a, n);
+	sort(a, a+n);
+	int ans = a[n-1]-a[0];
+	int small = a[0]+k;
+	int big = a[n-1] - k;
+	if(small > big){
+		swap(big, small);
+	}
+
+	for(int i=1; i<n-1; i++){
+		int sub = a[i] - k;
+		int add = a[i] + k;
+
+		if(sub >= small || add <= big){
+			continue;
+		}
+
+		if(big - sub >= small || add <= big){
+			small = sub;
+		}
+		else{
+			big = add;
+		}
+	}
+	cout<<min(ans, big - small)<<endl;
+}
+
+int main(){
+fastIO
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+	int t=1;cin>>t;
+	while(t--){
+		func();
+	}
+}			

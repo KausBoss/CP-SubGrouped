fbc2b99ba447cee0421fccd4ccf175349ea2f259
KausBoss <ktkaustubh4@gmail.com>
2020-08-15 16:16:28 +0530

    15 Aug

--- CodeForces/1398C.cpp
+++ CodeForces/1398C.cpp
@@ -1,94 +1,49 @@
 /*
-
 */
 #include <bits/stdc++.h>
 
 using namespace std;
 
 #define ll          long long int
-#define vi 			 vector<int>
-#define pb           push_back
-#define fi           first
-#define si           second
-#define pii 		 pair<int,int>
-#define vii 		 vector<pair<int,int>>
-#define F(a,n)       for(int i=0;i<n;i++){cin>>a[i];}
-#define P(a,n)       for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
-#define fastIO       ios_base::sync_with_stdio(false); cin.tie(NULL);
-#define F1(a,n)      for(int i=1;i<=n;i++){cin>>a[i];}
-#define P1(a,n)      for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
-#define mem(a,b)     memset(a, b, sizeof a)
-#define matrix(x) 	 vector<vector<x>>
-#define NF(a,n,m)    for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
-#define NF1(a,n,m)   for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
-#define PNF(a,n,m)   for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
-#define PNF1(a,n,m)  for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define mp          make_pair
+#define pb          push_back
+#define fi          first
+#define si          second
+#define fastIO      ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define F(a,n)      for(int i=0;i<n;i++){cin>>a[i];}
+#define F1(a,n)     for(int i=1;i<=n;i++){cin>>a[i];}
+#define P(a,n)      for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define P1(a,n)     for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define NF(a,n,m)   for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m)  for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m)  for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
 const int nax = 1e7;
 const int mod = 1e9+7;
 
-void func(){
-	ll n;cin>>n;
-	string s;
-	ll *a = new ll[n];
-	cin>>s;
-	for(int i=0; i<n; i++){
-		a[i] = (s[i]-'0');
-	}
-	ll total =0;
-	ll curSum=0;
-	ll len=0;
-	for(int i=0; i<n; i++){
-		if(curSum < len){
-			curSum += a[i];
-		}
-		else{
-			if(a[i] == 0){
-				len++;
-			}
-			else if(a[i] == 1 && curSum == len){
-				len++;
-				curSum++;
-			}
-			else{
-				len=1;
-				curSum = a[i];
-			}
-		}
-		// if(a[i] == 1){total++;}
-		if(curSum == len){total++;}
-	}
-	curSum =0, len=0;
-	for(int i=n-1; i>=0; i--){
-		if(curSum < len){
-			curSum += a[i];
-		}
-		else{
-			if(a[i] == 0){
-				len++;
-			}
-			else if(a[i] == 1 && curSum == len){
-				len++;
-				curSum++;
-			}
-			else{
-				len=1;
-				curSum = a[i];
-			}
-		}
-		// if(a[i] == 1){total++;}
-		if(curSum == len){total++;}
-	}
-	cout<<total<<endl;
-}
+
 
 int main(){
-fastIO
-#ifndef ONLINE_JUDGE
+	fastIO
+	#ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
-#endif
-	int t=1;cin>>t;
+	#endif
+	int t;
+	cin>>t;
 	while(t--){
-		func();
+		ll n;cin>>n;
+		string s;cin>>s;
+		map<ll, ll> m;
+		m[0] = 1;
+		ll total=0, sum=0;
+		for(int i=0; i<n; i++){
+			ll val = s[i] - '0';
+			val--;
+			sum += val;
+			total += m[sum];
+			m[sum]++;
+		}
+		cout<<total<<endl;
 	}
 }

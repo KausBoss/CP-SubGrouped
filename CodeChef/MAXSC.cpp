e991026fc77bf33e50872674060c2c6625cb5093
KausBoss <ktkaustubh4@gmail.com>
2020-08-17 18:37:35 +0530

    17 Aug

---
+++ CodeChef/MAXSC.cpp
@@ -0,0 +1,70 @@
+/*
+
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll          long long int
+#define vi 			 vector<int>
+#define pb           push_back
+#define fi           first
+#define si           second
+#define pii 		 pair<int,int>
+#define vii 		 vector<pair<int,int>>
+#define F(a,n)       for(int i=0;i<n;i++){cin>>a[i];}
+#define P(a,n)       for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define fastIO       ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define F1(a,n)      for(int i=1;i<=n;i++){cin>>a[i];}
+#define P1(a,n)      for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define mem(a,b)     memset(a, b, sizeof a)
+#define matrix(x) 	 vector<vector<x>>
+#define NF(a,n,m)    for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m)   for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m)   for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m)  for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+const int nax = 1e7;
+const int mod = 1e9+7;
+
+void func(){
+	ll n;
+	cin>>n;
+	vector<ll> a[n];
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			ll x;cin>>x;
+			a[i].pb(x);
+		}
+		sort(a[i].begin(), a[i].end());
+	}
+	ll ans = a[n-1][n-1], last = a[n-1][n-1];
+	ll picked = 1;
+
+	for(int i=n-2; i>=0; i--){
+		for(int j=n-1; j>=0; j--){
+			if(a[i][j] < last){
+				ans += a[i][j];
+				last = a[i][j];
+				picked++;
+				break;
+			}
+		}
+	}
+	if(picked == n){
+		cout<<ans<<"\n";
+		return;
+	} 
+	cout<<-1<<"\n";
+}
+
+int main(){
+	fastIO
+	#ifndef ONLINE_JUDGE
+	freopen("../inp.txt","r",stdin);
+    freopen("../out.txt","w",stdout);
+    #endif
+	int t=1;cin>>t;
+	while(t--){
+		func();
+	}
+}

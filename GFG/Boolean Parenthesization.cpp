a336546dc5f03369a5f3d389e8eb74f057cee0f9
KausBoss <ktkaustubh4@gmail.com>
2020-08-18 14:59:12 +0530

    18 Aug

--- GFG/Boolean Parenthesization.cpp
+++ GFG/Boolean Parenthesization.cpp
@@ -24,12 +24,71 @@ using namespace std;
 #define PNF(a,n,m)   for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
 #define PNF1(a,n,m)  for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
 const int nax = 1e7;
-const int mod = 1e9+7;
+const int mod = 1003;
+ll dp[101][101][2];
 string s;
-int n;
+ll n;
 
-ll func(int i){
-	
+ll func(ll i, ll j, bool ret){
+	//base case
+	if(j < i){
+		bool ret1 = (s[i-1]=='T');
+		return dp[i][j][ret] = (ret1 == ret);
+	}
+	if(i == j){
+		bool op1 = (s[i-1] == 'T');
+		bool op2 = (s[i+1] == 'T');
+		if(s[i] == '|'){return ((op1|op2) == ret);}
+		if(s[i] == '&'){return ((op1&op2) == ret);}
+		return dp[i][j][ret] = ((op1^op2) == ret);
+	}
+	//recursive case
+	if(dp[i][j][ret] != -1){ return dp[i][j][ret];}
+	ll ans = 0;
+	if(ret == 1){
+		for(int k=i; k<=j; k+=2){
+			if(k == '|'){
+				ans += (func(i, k-2, 1) * func(k+2, j, 1))%mod;
+				ans %= mod;
+				ans += (func(i, k-2, 1) * func(k+2, j, 0))%mod;
+				ans %= mod;
+				ans += (func(i, k-2, 0) * func(k+2, j, 1))%mod;
+				ans %= mod;
+			} 
+			else if(k == '&'){
+				ans += (func(i, k-2, 1) * func(k+2, j, 1))%mod;
+				ans %= mod;
+			}
+			else{
+				ans += (func(i, k-2, 1) * func(k+2, j, 0))%mod;
+				ans %= mod;
+				ans += (func(i, k-2, 0) * func(k+2, j, 1))%mod;
+				ans %= mod;
+			}
+		}
+		return dp[i][j][ret] = (ans%mod);
+	}
+	for(int k=i; k<=j; k+=2){
+		if(k == '|'){
+			ans += (func(i, k-2, 0) * func(k+2, j, 0))%mod;
+			ans %= mod;
+		} 
+		else if(k == '&'){
+			ans += (func(i, k-2, 0) * func(k+2, j, 0))%mod;
+			ans %= mod;
+			ans += (func(i, k-2, 1) * func(k+2, j, 0))%mod;
+			ans %= mod;
+			ans += (func(i, k-2, 0) * func(k+2, j, 1))%mod;
+			ans %= mod;
+		}
+		else{
+			ans += (func(i, k-2, 1) * func(k+2, j, 1))%mod;
+			ans %= mod;
+			ans += (func(i, k-2, 1) * func(k+2, j, 1))%mod;
+			ans %= mod;
+		}
+	}
+	return dp[i][j][ret] = (ans%mod);
 }
 
 int main(){
@@ -40,12 +99,9 @@ int main(){
     #endif
 	int t=1;cin>>t;
 	while(t--){
+		mem(dp, -1);
 		cin>>n;
 		cin>>s;
-		for(int i=0; i<n; i++){
-			if(i&1){sym.pb(s[i]);}
-			else{op.pb(s[i]);}
-		}
-		cout<<func()<<endl;
+		cout<<func(1, n-2, 1)<<endl;
 	}
 }

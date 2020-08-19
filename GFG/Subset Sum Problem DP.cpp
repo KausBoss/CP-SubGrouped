962e22b794b21f5735ff98905b226ee1173e36a6
KausBoss <ktkaustubh4@gmail.com>
2020-08-19 09:11:25 +0530

    19 Aug

---
+++ GFG/Subset Sum Problem DP.cpp
@@ -0,0 +1,62 @@
+/*
+
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll          long long int
+#define vi 			 vector<int>
+#define pb           push_back
+#define fi           first
+#define si           second
+#define pii 		 pair<int,int>
+#define vii 		 vector<pair<int,int>>
+#define F(a,n)       for(int i=0;i<n;i++){cin>>a[i];}
+#define P(a,n)       for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define fastIO       ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define F1(a,n)      for(int i=1;i<=n;i++){cin>>a[i];}
+#define P1(a,n)      for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define mem(a,b)     memset(a, b, sizeof a)
+#define matrix(x) 	 vector<vector<x>>
+#define NF(a,n,m)    for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m)   for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m)   for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m)  for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+const int nax = 1e7;
+const int mod = 1e9+7;
+
+bool func(){
+	ll n, t;
+	cin>>n>>t;
+	ll *a = new ll[n+1];
+	a[0] = 0;
+	F1(a, n);
+	vector<vector<bool>> dp(n+1, vector<bool>(t+1, 0));
+	for(int i=0; i<=n; i++){dp[i][0] = 1;}
+
+	for(int i=1; i<=n; i++){
+		for(int j=1; j<=t; j++){
+			if(a[i] > j){
+				dp[i][j] = dp[i-1][j];
+			}
+			else{
+				dp[i][j] = dp[i-1][j] | dp[i-1][j - a[i]];
+			}
+		}
+	}
+	// PNF(dp, n+1, t+1);
+	return dp[n][t];
+}
+
+int main(){
+	fastIO
+	#ifndef ONLINE_JUDGE
+	freopen("../inp.txt","r",stdin);
+    freopen("../out.txt","w",stdout);
+    #endif
+	int t=1;cin>>t;
+	while(t--){
+		cout<<func()<<endl;
+	}
+}

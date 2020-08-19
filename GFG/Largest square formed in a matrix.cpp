0f6a9fa39fea4259955067ed7f85454122c330f5
KausBoss <ktkaustubh4@gmail.com>
2020-08-19 07:51:33 +0530

    19 Aug

---
+++ GFG/Largest square formed in a matrix.cpp
@@ -0,0 +1,61 @@
+/*
+
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll          long long int
+#define vi 			 vector<int>
+#define pb           push_back
+#define fi           first
+#define si           second
+#define pii 		 pair<int,int>
+#define vii 		 vector<pair<int,int>>
+#define F(a,n)       for(int i=0;i<n;i++){cin>>a[i];}
+#define P(a,n)       for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define fastIO       ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define F1(a,n)      for(int i=1;i<=n;i++){cin>>a[i];}
+#define P1(a,n)      for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define mem(a,b)     memset(a, b, sizeof a)
+#define matrix(x) 	 vector<vector<x>>
+#define NF(a,n,m)    for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m)   for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m)   for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m)  for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+const int nax = 1e7;
+const int mod = 1e9+7;
+
+void func(){
+	ll n, m, ans=0;
+	cin>>n>>m;
+	vector<vector<int>> a(n, vector<int>(m));
+	NF(a, n, m);
+	for(int i=0; i<n; i++){
+		ans = max(ans, a[i][0]);
+	}
+	for(int i=0; i<m; i++){
+		ans = max(ans, a[0][i]);
+	}
+	for(int i=1; i<n; i++){
+		for(int j=1; j<m; j++){
+			if(a[i-1][j] && a[i][j-1] && a[i-1][j-1] && a[i][j]){
+				a[i][j] = 1 + min(a[i-1][j], min(a[i][j-1], a[i-1][j-1]));
+			}
+			ans = max(ans, (ll)a[i][j]);
+		}
+	}
+	cout<<ans<<endl;
+}
+
+int main(){
+	fastIO
+	#ifndef ONLINE_JUDGE
+	freopen("../inp.txt","r",stdin);
+    freopen("../out.txt","w",stdout);
+    #endif
+	int t=1;cin>>t;
+	while(t--){
+		func();
+	}
+}

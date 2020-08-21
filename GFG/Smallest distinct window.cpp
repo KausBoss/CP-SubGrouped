b83ada959cbc883897b1f8af8361ba7b996dae71
KausBoss <ktkaustubh4@gmail.com>
2020-08-21 18:03:43 +0530

    21 Aug

---
+++ GFG/Smallest distinct window.cpp
@@ -0,0 +1,67 @@
+/*
+
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll           long long int
+#define vi 			 vector<int>
+#define pb           push_back
+#define fi           first
+#define si           second
+#define pii 		 pair<int,int>
+#define vii 		 vector<pair<int,int>>
+#define F(a,n)       for(int i=0;i<n;i++){cin>>a[i];}
+#define P(a,n)       for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define fastIO       ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define F1(a,n)      for(int i=1;i<=n;i++){cin>>a[i];}
+#define P1(a,n)      for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define mem(a,b)     memset(a, b, sizeof a)
+#define matrix(x) 	 vector<vector<x>>
+#define NF(a,n,m)    for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m)   for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m)   for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m)  for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+const int nax = 1e7;
+const int mod = 1e9+7;
+
+void func(){
+	string s;
+	cin>>s;
+	int n = s.length();
+	vector<int> freq(26, 0);
+	int dist_char=0;
+	for(int i=0; i<n; i++){
+		if(freq[s[i] - 'a']==0){dist_char++;}
+		freq[s[i] - 'a']++;
+	}
+	freq = vector<int>(26, 0);
+	int cnt = 0, j=0;
+	int minSize = n;
+	for(int i=0; i<n; i++){
+		freq[s[i] - 'a']++;
+		if(freq[s[i] - 'a'] == 1){cnt++;}
+
+		//shrinking from left pointer
+		while(freq[s[j] - 'a'] > 1){
+			freq[s[j] - 'a']--;
+			j++;
+		}
+		if(cnt == dist_char){minSize = min (minSize, i-j+1);}
+		// cout<<j<<endl;
+	}
+	cout<<minSize<<endl;
+}
+
+int main(){
+	fastIO
+	#ifndef ONLINE_JUDGE
+	freopen("../inp.txt","r",stdin);
+    freopen("../out.txt","w",stdout);
+    #endif
+	int t=1;cin>>t;
+	while(t--){
+		func();
+	}
+}

34e55a0f7738e1c9a2bbec098d0a44b477530840
KausBoss <ktkaustubh4@gmail.com>
2020-08-22 06:17:24 +0530

    22 Aug

---
+++ GFG/Check if two strings are k-anagrams or not.cpp
@@ -0,0 +1,60 @@
+/*
+
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll           long long int
+#define vi 			 vector<int>
+#define pb           push_back
+#define fi           first
+#define si           second
+#define pii 		 pair<int,int>
+#define vii 		 vector<pair<int,int>>
+#define F(a,n)       for(int i=0;i<n;i++){cin>>a[i];}
+#define P(a,n)       for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define fastIO       ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define F1(a,n)      for(int i=1;i<=n;i++){cin>>a[i];}
+#define P1(a,n)      for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define mem(a,b)     memset(a, b, sizeof a)
+#define matrix(x) 	 vector<vector<x>>
+#define NF(a,n,m)    for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m)   for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m)   for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m)  for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+const int nax = 1e7;
+const int mod = 1e9+7;
+
+bool areKAnagrams(string str1, string str2, int k){
+	vector<int> freq(26, 0);
+	int n=str1.length(), m=str2.length();
+	for(int i=0; i<n; i++){
+		freq[str1[i]-'a']++;
+	}
+	for(int i=0; i<m; i++){
+		freq[str2[i]-'a']--;
+	}
+	int pos=0;
+	for(int i=0; i<26; i++){
+		if(freq[i] > 0){pos += freq[i];}
+	}
+	cout<<n<<" "<<m<<endl;
+	return (pos<=k) ? 1 : 0;
+}
+
+int main(){
+	fastIO
+	#ifndef ONLINE_JUDGE
+	freopen("../inp.txt","r",stdin);
+    freopen("../out.txt","w",stdout);
+    #endif
+	int t=1;cin>>t;
+	while(t--){
+		string s, t;
+		int k;
+		cin>>s>>t;
+		cin>>k;
+		cout<<areKAnagrams(s, t, k)<<endl;
+	}
+}

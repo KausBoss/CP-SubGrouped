4e33ba639e92a0bafab6e9f37f49b2115c2bbeb3
KausBoss <ktkaustubh4@gmail.com>
2020-08-22 14:40:46 +0530

    22 Aug

---
+++ CodeForces/1401D.cpp
@@ -0,0 +1,87 @@
+/*
+
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll           long long int
+#define vi 			 vector<int>
+#define pb           push_back
+#define fi           first
+#define si           second
+#define pii 		 pair<int,int>
+#define vii 		 vector<pair<int,int>>
+#define F(a,n)       for(int i=0;i<n;i++){cin>>a[i];}
+#define P(a,n)       for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define fastIO       ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define F1(a,n)      for(int i=1;i<=n;i++){cin>>a[i];}
+#define P1(a,n)      for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define mem(a,b)     memset(a, b, sizeof a)
+#define matrix(x) 	 vector<vector<x>>
+#define NF(a,n,m)    for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m)   for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m)   for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m)  for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+const int nax = 1e5 + 10;
+const int mod = 1e9+7;
+vector<list<ll>> g;
+vector<ll> times;
+ll visited[nax];
+ll n;
+
+ll dfs(ll node){
+	visited[node] = 1;
+	ll ret=1;
+	for(auto child:g[node]){
+		if(!visited[child]){
+			ll nt = dfs(child);
+			times.pb(nt * (n - nt));
+			ret += nt;
+		}
+	}
+	return ret;
+}
+
+int main(){
+	fastIO
+	#ifndef ONLINE_JUDGE
+	freopen("../inp.txt","r",stdin);
+    freopen("../out.txt","w",stdout);
+    #endif
+	int t=1;cin>>t;
+	while(t--){
+		cin>>n;
+		g = vector<list<ll>>(n);
+		mem(visited, 0);
+		for(int i=0; i<n-1; i++){
+			ll u, v;
+			cin>>u>>v;
+			u--;v--;
+			g[u].pb(v);
+			g[v].pb(u);
+		}
+		ll m;cin>>m;
+		vector<ll> fact(m);
+		F(fact, m);
+		while(fact.size() < n-1){fact.pb(1);}
+		sort(fact.begin(), fact.end());
+		while(fact.size() > n-1){
+			ll temp = fact.back();
+			fact.pop_back();
+			fact.back() *= temp;
+			fact.back() %= mod;
+		}
+		dfs(0);
+		sort(times.begin(), times.end());
+		ll ans = 0;
+		for(int i=0; i<n-1; i++){
+			ll temp = times[i]*fact[i];
+			temp %= mod;
+			ans += temp;
+			ans %= mod;
+		}
+		cout<<ans<<endl;
+		times.clear();
+	}
+}

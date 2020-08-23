a53795915a99780938c6b5d65e5f1e1fb44b3eda
KausBoss <ktkaustubh4@gmail.com>
2020-08-23 20:03:19 +0530

    23 Aug

---
+++ GFG/Count binary strings.cpp
@@ -0,0 +1,59 @@
+/*
+
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll           long long int
+#define vi 			 vector<int>
+#define pb           push_back
+#define fi           first
+#define si           second
+#define pii 		 pair<int,int>
+#define vii 		 vector<pair<int,int>>
+#define F(a,n)       for(int i=0;i<n;i++){cin>>a[i];}
+#define P(a,n)       for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define fastIO       ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define F1(a,n)      for(int i=1;i<=n;i++){cin>>a[i];}
+#define P1(a,n)      for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define mem(a,b)     memset(a, b, sizeof a)
+#define matrix(x) 	 vector<vector<x>>
+#define NF(a,n,m)    for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m)   for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m)   for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m)  for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+const int nax = 1e7;
+const int mod = 1e9+7;
+ll n, k;
+ll dp[1001][1001][2];
+
+ll func(ll i, ll k, bool last){
+	//base case
+	if(i == n){
+		return (k==0);
+	}
+	//recursive case
+	if(dp[i][k][last] != -1){return dp[i][k][last];}
+
+	ll op1 = func(i+1, k, 0);
+	ll op2 = 0;
+	if(!last || (last && k)){
+		op2 = func(i+1, k - (last), 1);
+	}
+	return dp[i][k][last] = (op1 + op2)%mod;
+}
+
+int main(){
+	fastIO
+	#ifndef ONLINE_JUDGE
+	freopen("../inp.txt","r",stdin);
+    freopen("../out.txt","w",stdout);
+    #endif
+	int t=1;cin>>t;
+	while(t--){
+		mem(dp, -1);
+		cin>>n>>k;
+		cout<<func(0, k, 0)<<endl;
+	}
+}

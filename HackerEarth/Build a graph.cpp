2a52c16720115650fd5ab36354d5beea02eb9ca4
KausBoss <ktkaustubh4@gmail.com>
2020-08-30 13:34:29 +0530

    30 Aug

--- HackerEarth/Build a graph.cpp
+++ HackerEarth/Build a graph.cpp
@@ -4,6 +4,10 @@
 using namespace std;
 
 int main(){
+	#ifndef ONLINE_JUDGE
+	freopen("../inp.txt","r",stdin);
+	freopen("../out.txt","w",stdout);
+	#endif
     int n;
     cin>>n;
     if(n >= 12){

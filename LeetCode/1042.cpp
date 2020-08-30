4b9f4d14c4853addd5836ab7b820e7b4958983d4
KausBoss <ktkaustubh4@gmail.com>
2020-08-30 15:52:15 +0530

    30 Aug

--- LeetCode/1042.cpp
+++ LeetCode/1042.cpp
@@ -1,17 +1,41 @@
 class Solution {
 public:
-    int findJudge(int N, vector<vector<int>>& trust) {
-        vector<int> t(N+1, 0);
-        vector<set<int>> here(N+1);
-        for(auto x: trust){
-        	t[x[0]]++;
-        	here[x[1]].insert(x[0]);
+    vector<list<int>> g;
+    vector<bool> visited;
+    vector<int> ans;
+    void dfs(int node){
+        visited[node]=1;
+        bool col[5] = {0, 1, 1, 1, 1};
+        for(auto child:g[node]){
+            if(ans[child] != 0){
+                col[ans[child]]=0;
+            }
         }
-        for(int i=1; i<=N; i++){
-        	if(here[i].size() == N-1 && t[i]==0){
-        		return i;
-        	}
+        for(int i=1; i<=4; i++){
+            if(col[i]){
+                ans[node] = i;
+                break;
+            }
         }
-        return -1;
+        for(auto child:g[node]){
+            if(!visited[child]){
+                dfs(child);
+            }
+        }
+    }
+    vector<int> gardenNoAdj(int N, vector<vector<int>>& paths) {
+        g.resize(N);
+        visited = vector<bool>(N, 0);
+        ans = vector<int>(N, 0);
+        for(auto x:paths){
+            g[x[0]-1].push_back(x[1]-1);
+            g[x[1]-1].push_back(x[0]-1);
+        }
+        for(int i=0; i<N; i++){
+            if(!visited[i]){
+                dfs(i);
+            }
+        }
+        return ans;
     }
 };

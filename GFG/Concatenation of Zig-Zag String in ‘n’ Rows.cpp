b1abe554bcf065cf31af00e2574c3b8547e14202
KausBoss <ktkaustubh4@gmail.com>
2020-09-02 23:27:14 +0530

    2 Sept

---
+++ GFG/Concatenation of Zig-Zag String in ‘n’ Rows.cpp
@@ -0,0 +1,70 @@
+/*
+
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll           long long int
+#define vi 			 vector<int>
+#define pb           push_back
+#define fi           first
+#define si           second
+#define pii 		 pair<int,int>
+#define vii 		 vector<pair<int,int>>
+#define F(a,n)       for(int i=0;i<n;i++){cin>>a[i];}
+#define P(a,n)       for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define fastIO       ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define F1(a,n)      for(int i=1;i<=n;i++){cin>>a[i];}
+#define P1(a,n)      for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define mem(a,b)     memset(a, b, sizeof a)
+#define matrix(x) 	 vector<vector<x>>
+#define NF(a,n,m)    for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m)   for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m)   for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m)  for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+const int nax = 1e7;
+const int mod = 1e9+7;
+
+void func(){
+	string s;
+	cin>>s;
+	int n = s.length();
+	int k;
+	cin>>k;
+	if(k==1){
+		cout<<s<<endl;
+		return;
+	}
+	vector<string> ans(k);
+	int pt = 0, chng=1;
+	for(int i=0; i<n; i++){
+		ans[pt] += s[i];
+		pt += chng;
+		if(pt<0){
+			pt = 1	;
+			chng = 1;
+		}
+		else if(pt == k){
+			pt = k-2;
+			chng = -1;
+		}
+	}
+	
+	for(int i=0; i<k; i++){
+		cout<<ans[i];
+	}
+	cout<<"\n";
+}
+
+int main(){
+	fastIO
+	#ifndef ONLINE_JUDGE
+	freopen("../inp.txt","r",stdin);
+    freopen("../out.txt","w",stdout);
+    #endif
+	int t=1;cin>>t;
+	while(t--){
+		func();
+	}
+}

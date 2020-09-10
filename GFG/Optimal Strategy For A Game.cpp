7c946e2de105b7d6f7603fe5ec0edc8047f57735
KausBoss <ktkaustubh4@gmail.com>
2020-09-10 18:17:57 +0530

    10 Sept

---
+++ GFG/Optimal Strategy For A Game.cpp
@@ -0,0 +1,58 @@
+/*
+
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll           long long int
+#define vi 			 vector<int>
+#define pb           push_back
+#define fi           first
+#define si           second
+#define pii 		 pair<int,int>
+#define vii 		 vector<pair<int,int>>
+#define F(a,n)       for(int i=0;i<n;i++){cin>>a[i];}
+#define P(a,n)       for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define fastIO       ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define F1(a,n)      for(int i=1;i<=n;i++){cin>>a[i];}
+#define P1(a,n)      for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define mem(a,b)     memset(a, b, sizeof a)
+#define matrix(x) 	 vector<vector<x>>
+#define NF(a,n,m)    for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m)   for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m)   for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m)  for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+const int nax = 1e7;
+const int mod = 1e9+7;
+ll a[100], dp[100][100][2], n;
+
+ll func(int i, int j, bool turn){
+	//base case
+	if(i==j){
+		return (turn) ? a[i] : 0;
+	}
+	//recursive case
+	if(dp[i][j][turn] != -1){ return dp[i][j][turn];}
+	
+	if(turn){
+		return dp[i][j][turn] = max(a[i] + func(i+1, j, 0), a[j] + func(i, j-1, 0));
+	}
+	
+	return dp[i][j][turn] = min(func(i+1, j, 1), func(i, j-1, 1));
+}
+
+int main(){
+	fastIO
+	#ifndef ONLINE_JUDGE
+	freopen("../inp.txt","r",stdin);
+    freopen("../out.txt","w",stdout);
+    #endif
+	int t=1;cin>>t;
+	while(t--){
+		mem(dp, -1);
+		cin>>n;
+		F(a, n);
+		cout<<func(0, n-1, 1)<<endl;
+	}
+}

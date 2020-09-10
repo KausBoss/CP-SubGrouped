ca330a5c1fc12a456e5e05c5b30c5755509d5f67
KausBoss <ktkaustubh4@gmail.com>
2020-09-10 12:45:31 +0530

    10 Sept

---
+++ LeetCode/September Challenge/10.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        int n = secret.length();
+        int bulls=0, cows=0;
+        for(int i=0; i<n; i++){
+        	if(secret[i] == guess[i]){
+        		bulls++;
+        		secret[i] = 'B';
+                guess[i] = 'B';
+        	}
+        }
+        int freq[10] = {0};
+        for(int i=0; i<n; i++){
+        	if(secret[i] >= '0' && secret[i] <= '9'){
+        		freq[secret[i] - '0']++;
+        	}
+        }
+        for(int i=0; i<n; i++){
+        	if(guess[i] != 'B'){
+	        	int digit = guess[i] - '0';
+	        	if(freq[digit] > 0){
+	        		cows++;
+	        		freq[digit]--;
+	        	}
+        	}
+        }
+        return to_string(bulls) + "A" + to_string(cows) + "B";
+    }
+};

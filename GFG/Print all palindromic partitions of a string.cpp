32a6d8644f1e2176c71ce046c2e8a0d5f7de8350
KausBoss <ktkaustubh4@gmail.com>
2020-09-13 21:49:04 +0530

    13 Sept

---
+++ GFG/Print all palindromic partitions of a string.cpp
@@ -0,0 +1,74 @@
+/*
+
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll           long long int
+#define vi 			 vector<int>
+#define pb           push_back
+#define fi           first
+#define si           second
+#define pii 		 pair<int,int>
+#define vii 		 vector<pair<int,int>>
+#define F(a,n)       for(int i=0;i<n;i++){cin>>a[i];}
+#define P(a,n)       for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define fastIO       ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define F1(a,n)      for(int i=1;i<=n;i++){cin>>a[i];}
+#define P1(a,n)      for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define mem(a,b)     memset(a, b, sizeof a)
+#define matrix(x) 	 vector<vector<x>>
+#define NF(a,n,m)    for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m)   for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m)   for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m)  for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+const int nax = 1e7;
+const int mod = 1e9+7;
+int n;
+string s;
+
+bool isplain(string t){
+	int i=0, j=t.length()-1;
+	while(i < j){
+		if(t[i] != t[j]){
+			return 0;
+		}
+		i++;j--;
+	}
+	return 1;
+}
+
+void func(int i, vector<string> v){
+	//base case
+	if(i == n){
+		for(auto x: v){cout<<x<<" ";}
+		cout<<"\n";
+		return;
+	}
+	//recursive case
+	string sub = "";
+	for(int j=i; j<n; j++){
+		sub += s[j];
+		if(isplain(sub)){
+			v.pb(sub);
+			func(j+1, v);
+			v.pop_back();
+		}
+	}
+}
+
+
+int main(){
+	fastIO
+	#ifndef ONLINE_JUDGE
+	freopen("../inp.txt","r",stdin);
+    freopen("../out.txt","w",stdout);
+    #endif
+	int t=1;cin>>t;
+	while(t--){
+		cin>>s;
+		n = s.length();
+		func(0, {});
+	}
+}

f54d2944d2a00225c9a88c5b7c1a7f94fb902642
KausBoss <ktkaustubh4@gmail.com>
2020-10-03 04:10:49 +0530

    2 Oct

---
+++ roughCodes/Coin Change Permutations.cpp
@@ -0,0 +1,65 @@
+/*
+
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll           		long long int
+#define vi 				 	vector<int>
+#define pb      	     	push_back
+#define fi          	 	first
+#define si      	     	second
+#define pii		 		 	pair<int,int>
+#define vii 			 	vector<pair<int,int>>
+#define F(a,n)  	     	for(int i=0;i<n;i++){cin>>a[i];}
+#define P(a,n) 		    	for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define fastIO      	 	ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define F1(a,n) 	     	for(int i=1;i<=n;i++){cin>>a[i];}
+#define P1(a,n)     		for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define mem(a,b)	     	memset(a, b, sizeof a)
+#define matrix(x) 		 	vector<vector<x>>
+#define NF(a,n,m)      	    for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m)		    for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m)  	 	for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m)  		for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define ceil_div(x, y) 		(((x) + (y) - 1) / (y))
+const int nax = 1e7;
+const int mod = 1e9+7;
+int n, dp[31];
+vector<int> a;
+
+int func(int amt){
+	//base case
+	if(amt == 0){return 1;}
+	//recursive case
+	if(dp[amt] != -1){return dp[amt];}
+	int ways = 0;
+	for(int i=0; i<n; i++){
+		if(a[i] > amt){break;}
+		ways += func(amt - a[i]);
+	}
+	return dp[amt] = ways;
+}
+
+int main(){
+	fastIO
+	#ifndef ONLINE_JUDGE
+	freopen("../inp.txt","r",stdin);
+    freopen("../out.txt","w",stdout);
+    #endif
+	int amt, temp;
+	cin>>n;
+	bitset<21> freq;
+	for(int i=0; i<n; i++){
+		cin>>temp;
+		freq[temp] = 1;
+	}
+	for(int i=1; i<=20; i++){
+		if(freq[i]){a.pb(i);}
+	}
+	n = a.size();
+	cin>>amt;
+	mem(dp, -1);
+	cout<<func(amt);
+}

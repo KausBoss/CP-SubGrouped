2d3253a831735ef45e8990790906b3907d8fc62a
KausBoss <ktkaustubh4@gmail.com>
2020-10-15 22:01:54 +0530

    15 Oct

---
+++ roughCodes/Spread Of Infection.cpp
@@ -0,0 +1,65 @@
+/*
+
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll           		long long int
+#define vi 				 	vector<int>
+#define pb      	     	push_back
+#define fi          	 	first
+#define si      	     	second
+#define pii		 		 	pair<int,int>
+#define vii 			 	vector<pair<int,int>>
+#define F(a,n)  	     	for(int i=0;i<n;i++){cin>>a[i];}
+#define P(a,n) 		    	for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define fastIO      	 	ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define F1(a,n) 	     	for(int i=1;i<=n;i++){cin>>a[i];}
+#define P1(a,n)     		for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define mem(a,b)	     	memset(a, b, sizeof a)
+#define matrix(x) 		 	vector<vector<x>>
+#define NF(a,n,m)      	    for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m)		    for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m)  	 	for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m)  		for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define ceil_div(x, y) 		(((x) + (y) - 1) / (y))
+const int nax = 1e7;
+const int mod = 1e9+7;
+
+int main(){
+	fastIO
+	#ifndef ONLINE_JUDGE
+	freopen("../inp.txt","r",stdin);
+    freopen("../out.txt","w",stdout);
+    #endif
+	int n, m, src, t, ans=1;
+	vector<int> g[10];
+	cin>>n>>m;
+	for(int i=0; i<m; i++){
+		int x, y, w;
+		cin>>x>>y>>w;
+		g[x].pb(y);
+		g[y].pb(x);
+	}
+	cin>>src>>t;
+	vector<bool> visited(10, 0); 
+	queue<pair<int,int>> q;
+	q.push({src, 1});
+	visited[src] = 1;
+	while(!q.empty()){
+		pair<int,int> node = q.front();
+		q.pop();
+		if(node.si < t){
+			for(auto child: g[node.fi]){
+				if(visited[child] == 0){
+					q.push({child, node.si + 1});
+					visited[child] = 1;
+					// cout<<child<<" "<<node.fi<<endl;
+					ans++;
+				}
+			}
+		}
+	}
+	cout<<ans;
+}

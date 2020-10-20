68c4d0a2e3f08b54cf812fd680b3cb7ca8e678ad
KausBoss <ktkaustubh4@gmail.com>
2020-10-20 22:25:54 +0530

    20 Oct

---
+++ CodeForces/1433C.cpp
@@ -0,0 +1,73 @@
+/*
+
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll           		long long int
+#define vi 				 	vector<int>
+#define pb      	     	push_back
+#define fi          	 	first
+#define si      	     	second
+#define pii		 		 	pair<int,int>
+#define vii 			 	vector<pair<int,int>>
+#define F(a,n)  	     	for(int i=0;i<n;i++){cin>>a[i];}
+#define P(a,n) 		    	for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define fastIO      	 	ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define F1(a,n) 	     	for(int i=1;i<=n;i++){cin>>a[i];}
+#define P1(a,n)     		for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define mem(a,b)	     	memset(a, b, sizeof a)
+#define matrix(x) 		 	vector<vector<x>>
+#define NF(a,n,m)      	    for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m)		    for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m)  	 	for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m)  		for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define ceil_div(x, y) 		(((x) + (y) - 1) / (y))
+const int nax = 3e5 + 3;
+const int mod = 1e9+7;
+ll a[nax];
+
+
+void func(){
+	ll n;
+	cin>>n;
+	F(a, n);
+	bool diff=0;
+	for(int i=1; i<n; i++){
+		if(a[i] != a[0]){diff=1;}
+	}
+	if(diff == 0){
+		cout<<-1<<"\n";
+		return;
+	}
+	ll mxsize=-1;
+	for(int i=0; i<n; i++){
+		mxsize = max(mxsize, a[i]);
+	}
+	ll ans;
+	for(int i=0; i<n; i++){
+		if(a[i] == mxsize){
+			if(i+1<n && a[i+1]<mxsize){
+				cout<<i+1<<"\n";
+				return;
+			}
+			if(i-1>=0 && a[i-1] < mxsize){
+				cout<<i+1<<"\n";
+				return;
+			}
+		}
+	}
+}
+
+int main(){
+	fastIO
+	#ifndef ONLINE_JUDGE
+	freopen("../inp.txt","r",stdin);
+    freopen("../out.txt","w",stdout);
+    #endif
+	int t=1;cin>>t;
+	while(t--){
+		func();
+	}
+}

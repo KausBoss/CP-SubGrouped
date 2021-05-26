e318b31a4cc5b56de0111f6542e7ca7b5dd0a450
KausBoss <ktkaustubh4@gmail.com>
2021-05-26 22:45:10 +0530

    26 May

---
+++ CSES/Repetitions.cpp
@@ -0,0 +1,60 @@
+/*
+
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll           		long long int
+#define vi 				 	vector<int>
+#define pb      	     	push_back
+#define fi          	 	first
+#define si      	     	second
+#define pii		 		 	pair<int,int>
+#define vii 			 	vector<pair<int,int>>
+#define F(a,n)  	     	for(int i=0;i<n;i++){cin>>a[i];}
+#define P(a,n) 		    	for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define fastIO      	 	ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define F1(a,n) 	     	for(int i=1;i<=n;i++){cin>>a[i];}
+#define P1(a,n)     		for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define mem(a,b)	     	memset(a, b, sizeof a)
+#define matrix(x) 		 	vector<vector<x>>
+#define NF(a,n,m)      	    for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m)		    for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m)  	 	for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m)  		for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define ceil_div(x, y) 		(((x) + (y) - 1) / (y))
+const int nax = 1e7;
+const int mod = 1e9+7;
+
+void func(){
+	string s;
+	cin>>s;
+	char lastChar = s[0];
+	int maxLen = 0, curLen=1;
+	for(int i=1; i<s.length();i++){
+		// cout<<s[i]<<" "<<lastChar<<" "<<maxLen<<" "<<curLen<<endl;
+		if(s[i] == lastChar){
+			curLen++;
+		}
+		else{
+			lastChar = s[i];
+			maxLen = max(maxLen, curLen);
+			curLen = 1;
+		}
+	}
+	maxLen = max(maxLen, curLen);
+	cout<<maxLen;
+}
+
+int main(){
+	fastIO
+	#ifndef ONLINE_JUDGE
+	freopen("../inp.txt","r",stdin);
+    freopen("../out.txt","w",stdout);
+    #endif
+	int t=1;//cin>>t;
+	while(t--){
+		func();
+	}
+}

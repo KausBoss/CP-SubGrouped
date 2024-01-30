f66b073c82d7c0f6f5d9f4a4f8486726b6f5d12a
KausBoss <ktkaustubh4@gmail.com>
2024-01-29 20:47:11 -0700

    29 Jan

--- CSES/Weird_Algorithm.cpp
+++ CSES/Weird_Algorithm.cpp
@@ -27,22 +27,17 @@ using namespace std;
 const int nax = 1e7;
 const int mod = 1e9+7;
 
-void func(){
-	long n;
+void func() {
+	ll n;
 	cin>>n;
-	cout<<n<<" ";
-	while(n != 1){
-		if(n%2){
-			n *= 3;
-			n++;
-		}
-		else{
-			n /= 2;	
-		}
+	while (n != 1) {
 		cout<<n<<" ";
+		if (n&1) n = n*3 + 1;
+		else n /= 2;
 	}
-	
+	cout<<1;
 }
+
 
 int main(){
 	fastIO
@@ -50,8 +45,5 @@ int main(){
 	freopen("../inp.txt","r",stdin);
     freopen("../out.txt","w",stdout);
     #endif
-	int t=1;//cin>>t;
-	while(t--){
-		func();
-	}
+    func();
 }
